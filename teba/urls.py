848027dd2502b531054a8331ef1ae9f67eb487ed
kagzpro <rkagga07@gmail.com>
2026-05-30 01:19:51 -0700

    Update urls.py

--- teba/urls.py
+++ teba/urls.py
@@ -1,23 +1,20 @@
 # teba/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView, RedirectView
+from django.views.generic import TemplateView
 from django.shortcuts import redirect
 
 from core.views import CustomLoginView
 
 
 urlpatterns = [
-    # Root redirect - goes to login page
-    path('', RedirectView.as_view(url='/accounts/login/', permanent=False), name='home'),
-    
     path('admin/', admin.site.urls),
 
     # Language switch endpoint (MUST be near the top)
     path('i18n/', include('django.conf.urls.i18n')),
 
     # Apps
-    path('transactions/', include('transactions.urls', namespace='transactions')),
+    path('', include('transactions.urls', namespace='transactions')),
     path('core/', include('core.urls', namespace='core')),
     path('inventory/', include('inventory.urls', namespace='inventory')),
 
@@ -29,7 +26,8 @@ urlpatterns = [
 
     # Home page
     path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
-    
-    # Rosetta (translations - optional)
-    path('rosetta/', include('rosetta.urls')),
+
+    # Root redirect
+    path('', lambda request: redirect('transactions:home')),
+     path('rosetta/', include('rosetta.urls')),
 ]
